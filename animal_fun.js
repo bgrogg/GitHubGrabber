5bcf28b1372a6f07db331c1c3f17079062e9aa55
bgrogg <bgrogg3@gmail.com>
2017-09-22 17:08:42 -0700

    wrote selectAnimals to read and write files by first letter

--- animal_fun.js
+++ animal_fun.js
@@ -1,9 +1,47 @@
 const fs = require('fs');
 
-fs.readFile('./animals.txt', (err, data) => {
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//   console.log(data);
+//   }
+// })
+
+
+
+// CAREFUl: If a file already exists as the first argument, it will be
+// overwritten!
+// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file successfully written!");
+//   }
+// })
+
+const firstLetter = process.argv[2].toUpperCase();
+
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-    return;
+    return
   }
-  console.log(data);
+  const animals = selectAnimals(data, firstLetter)
+
+  fs.writeFile(`${firstLetter}_animals.txt`, animals, err => {
+    if (err) {
+      console.log(err);
+      return
+    } else {
+      console.log("file successfully written!")
+    }
+  })
 })
+
+function selectAnimals(animal, firstLetter) {
+  return animal
+    .split('\n')
+    .filter(animal => animal.startsWith(firstLetter))
+    .join('\n')
+}
